dabb7cabbbf1db4b0bab13ccbcc318ced8fe1de5
Ungsik Yun <ungsik.yun@gmail.com>
2018-09-17 12:43:31 +0900

    hello.py modified on Local repository

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
+// For command line git tutorial
+// Local repository modification
+
 print("Hello world")
 print("Tell Your World")
-print("Tell this world")
+print("Tell his world")
 print("Tell her world")
